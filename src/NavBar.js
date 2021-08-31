e73deb3cab0e35c2190ce4a17948c1fbd855ff56
Lia Ximenes <ximeneslia@gmail.com>
2021-08-31 00:19:14 -0300

    navbar implemented

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,6 +1,25 @@
+import styled from 'styled-components';
+import logo from "./images/nutcache-logo.png"
+
 export default function NavBar(){
     return (
-        <>
-        </>
+        <Bar>
+            <img src={logo}/>
+        </Bar>
     )
 }
+
+const Bar = styled.div`
+    width: 100%;
+    height: 100px;
+    display: flex;
+    align-items: center;
+    box-shadow: 0px 5px 10px 0px #C8C2BC;
+
+
+    img{
+        height: 75px;
+        margin-left: 10px;
+    }
+`;
+

77cfc25226b43b6c1678d4d9efe4d8ac480dd512
Lia Ximenes <ximeneslia@gmail.com>
2021-08-30 22:40:34 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import NavBar from "./NavBar";
+import MainPage from "./MainPage";
+import Footer from "./Footer";
+
+export default function App(){
+    return(
+        <>
+            <NavBar/>
+            <MainPage/>
+            <Footer/>
+        </>
+    )
+}
